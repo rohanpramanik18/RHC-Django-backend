3f53d8ca4c66e268618ad613093d0f8aa28a77cc
unknown <rohan.pramanik01@gmail.com>
2022-12-15 23:40:33 +0530

    initial commit1

--- rural_health/urls.py
+++ rural_health/urls.py
@@ -18,12 +18,8 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
-from django.views.static import serve
-from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(("userdata.urls", 'userdata') , namespace='userdata')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
